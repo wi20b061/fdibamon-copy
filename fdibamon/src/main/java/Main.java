8cdf459841a71acc86c4d27205a1eb81ff842487
nkutsev <nikiforte@abv.bg>
2022-10-14 15:46:06 +0300

    Story 1 undone - couple of things to fix.

--- fdibamon/src/main/java/Main.java
+++ fdibamon/src/main/java/Main.java
@@ -1,2 +1,43 @@
+import entity.Fdibamon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        List<Fdibamon> fdibamons = new ArrayList<>();
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        fdibamons.add(new Fdibamon("Torud", 50, 20));
+        fdibamons.add(new Fdibamon("Morzith", 40, 50));
+        fdibamons.add(new Fdibamon("Unoth", 60, 30));
+        fdibamons.add(new Fdibamon("Gandalf", 50, 40));
+        fdibamons.add(new Fdibamon("Mordekeiser", 70, 70));
+
+        printHelloMessage();
+        int counter = 1;
+        int userInput;
+        //TODO: fix the do while loop
+        do {
+            for (Fdibamon fdibamon : fdibamons) {
+                System.out.printf("%d. %-15s | Hit Points: %5d | Attack Power: %5d%n",
+                        counter++,
+                        fdibamon.getName(),
+                        fdibamon.getHitPoints(),
+                        fdibamon.getAttackPower());
+            }
+            userInput = Integer.parseInt(bf.readLine());
+        } while (userInput <= 0 && userInput >= 5);
+
+
+
+    }
+
+    private static void printHelloMessage() {
+        System.out.println("Welcome. Please, pick a valid fdibamon: ");
+    }
 }
